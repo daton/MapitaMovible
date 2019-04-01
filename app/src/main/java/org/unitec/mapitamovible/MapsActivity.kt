face0025ab05401ace24ab529b464f9f1c4742b1
campitos <rapidclimate@gmail.com>
2019-03-31 22:46:12 -0600

    Initial commit

---
+++ app/src/main/java/org/unitec/mapitamovible/MapsActivity.kt
@@ -0,0 +1,225 @@
+package org.unitec.mapitamovible
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+import android.widget.TextView
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.maps.CameraUpdateFactory
+
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.ArrayList
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+    LocationListener {
+
+    private lateinit var mMap: GoogleMap
+
+
+    private var mylocation: Location? = null
+    private var googleApiClient: GoogleApiClient? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        setUpGClient()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+      //  mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+
+    @Synchronized
+    private fun setUpGClient() {
+        googleApiClient = GoogleApiClient.Builder(this)
+            .enableAutoManage(this, 0, this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
+        googleApiClient!!.connect()
+    }
+
+    override fun onLocationChanged(location: Location) {
+        mylocation = location
+        if (mylocation != null) {
+            val latitude = mylocation!!.latitude
+            val longitude = mylocation!!.longitude
+            //latitudeTextView!!.text = "Latitude : $latitude"
+           // longitudeTextView!!.text = "Longitude : $longitude"
+            //Or Do whatever you want with your location
+            val sydney = LatLng(latitude, longitude)
+       //     mMap.addMarker(MarkerOptions().position(sydney).title("aqui..."))
+          //   var circle = mMap.addCircle( CircleOptions()
+    // .center(sydney)
+    // .radius(0.5)
+    // .strokeColor(Color.RED)
+     //.fillColor(Color.BLUE));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(19f))
+        }
+    }
+
+    override fun onConnected(bundle: Bundle?) {
+        checkPermissions()
+    }
+
+    override fun onConnectionSuspended(i: Int) {
+        //Do whatever you need
+        //You can display a message here
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        //You can display a message here
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getMyLocation() {
+        if (googleApiClient != null) {
+            if (googleApiClient!!.isConnected) {
+                val permissionLocation = ContextCompat.checkSelfPermission(this@MapsActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
+                    mylocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient)
+                    val locationRequest = LocationRequest()
+                    locationRequest.interval = 3000
+                    locationRequest.fastestInterval = 3000
+                    locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                    val builder = LocationSettingsRequest.Builder()
+                        .addLocationRequest(locationRequest)
+                    builder.setAlwaysShow(true)
+                    LocationServices.FusedLocationApi
+                        .requestLocationUpdates(googleApiClient, locationRequest, this)
+                    val result = LocationServices.SettingsApi
+                        .checkLocationSettings(googleApiClient, builder.build())
+                    result.setResultCallback { result ->
+                        val status = result.status
+                        when (status.statusCode) {
+                            LocationSettingsStatusCodes.SUCCESS -> {
+                                // All location settings are satisfied.
+                                // You can initialize location requests here.
+                                val permissionLocation = ContextCompat
+                                    .checkSelfPermission(this@MapsActivity,
+                                        Manifest.permission.ACCESS_FINE_LOCATION)
+                                if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
+                                    mylocation = LocationServices.FusedLocationApi
+                                        .getLastLocation(googleApiClient)
+                                }
+                            }
+                            LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                                // Location settings are not satisfied.
+                                // But could be fixed by showing the user a dialog.
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(),
+                                    // and check the result in onActivityResult().
+                                    // Ask to turn on GPS automatically
+                                    status.startResolutionForResult(this@MapsActivity,
+                                        REQUEST_CHECK_SETTINGS_GPS)
+                                } catch (e: IntentSender.SendIntentException) {
+                                    // Ignore the error.
+                                }
+                            }
+
+                            LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                            }
+                        }// Location settings are not satisfied.
+                        // However, we have no way
+                        // to fix the
+                        // settings so we won't show the dialog.
+                        // finish();
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_CHECK_SETTINGS_GPS -> when (resultCode) {
+                Activity.RESULT_OK -> getMyLocation()
+                Activity.RESULT_CANCELED -> finish()
+            }
+        }
+    }
+
+    private fun checkPermissions() {
+        val permissionLocation = ContextCompat.checkSelfPermission(this@MapsActivity,
+            android.Manifest.permission.ACCESS_FINE_LOCATION)
+        val listPermissionsNeeded = ArrayList<String>()
+        if (permissionLocation != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            if (!listPermissionsNeeded.isEmpty()) {
+                ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toTypedArray(), REQUEST_ID_MULTIPLE_PERMISSIONS)
+            }
+        } else {
+            getMyLocation()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        val permissionLocation = ContextCompat.checkSelfPermission(this@MapsActivity,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+        if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
+            getMyLocation()
+        }
+    }
+
+    companion object {
+        private val REQUEST_CHECK_SETTINGS_GPS = 0x1
+        private val REQUEST_ID_MULTIPLE_PERMISSIONS = 0x2
+    }
+}
